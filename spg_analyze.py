b1f89c4ef09bffdbe73094295727d6854f98ee02
Alexander Senier <alexander.senier@tu-dresden.de>
2016-04-25 22:11:16 +0200

    Initial z3 prover support

--- spg_analyze.py
+++ spg_analyze.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import xml.etree.ElementTree as ET
 import networkx as nx
@@ -6,26 +6,11 @@ import sys
 import argparse
 import subprocess
 import os
+from z3 import *
 
 iteration = 0
 output = ""
 pdfs = []
-
-free_arguments = {
-    'xform':        None,
-    'guard':        ['data'],
-    'hash':         ['msg'],
-    'verify_hash':  ['msg'],
-    'hmac':         ['msg'],
-    'hmac_inline':  ['msg'],
-    'verify_hmac':  ['msg'],
-    'sign':         ['msg'],
-    'verify_sig':   ['msg'],
-    'dhsec':        ['pub'],
-    'encrypt':      ['plaintext'],
-    'decrypt':      ['ciphertext'],
-    'release':      ['data']
-}
 
 def sec_empty():
     return set(())
@@ -85,7 +70,8 @@ def freeze(s):
 
 def fmtsec(sec):
     if not sec:
-        return "&empty;"
+        #return "&empty;"
+        return "{}"
     else:
         result = ""
         if "c" in sec:
@@ -162,17 +148,6 @@ def set_outputs (G, node, argmap):
             data['sec'] = argmap[sarg]
             changes += 1
 
-        # if data['sec'] < argmap[sarg]:
-        #     # Upgrading is always OK, as it just increases the security requirements imposed on the environment
-        #     data['sec'] = argmap[sarg]
-        #     changes += 1
-        # elif data['sec'] > argmap[sarg]:
-        #     kind = G.node[child]['kind']
-        #     # Downgrade is only allowed for 'free' parameters
-        #     if kind in free_arguments and (free_arguments[kind] == None or darg in free_arguments[kind]):
-        #         data['sec'] = argmap[sarg]
-        #         changes += 1
-
         seen[sarg] = True
 
     for s in seen:
@@ -182,6 +157,39 @@ def set_outputs (G, node, argmap):
         raise Exception, "Node '" + node + "' has no arguments " + str(list(argmap.keys()))
 
     return changes
+
+def set_pre (G, node, argmap):
+    for (parent, current, data) in G.in_edges (nbunch=node, data=True):
+        darg = data['darg']
+
+        if not darg in argmap:
+            raise Exception, "Setting invalid pred '" + darg + "' for node " + node
+
+        if argmap[darg] == None:
+            data['pre_c'] = Bool(current + "_pre_c")
+            data['pre_i'] = Bool(current + "_pre_i")
+            data['pre_f'] = Bool(current + "_pre_f")
+        else:
+            data['pre_c'] = Bool(True) if 'c' in argmap[darg] else Bool(current + "_pre_c")
+            data['pre_i'] = Bool(True) if 'i' in argmap[darg] else Bool(current + "_pre_i")
+            data['pre_f'] = Bool(True) if 'f' in argmap[darg] else Bool(current + "_pre_f")
+
+def set_post (G, node, argmap):
+    
+    for (current, child, data) in G.out_edges (nbunch=node, data=True):
+        sarg = data['sarg']
+
+        if not sarg in argmap:
+            raise Exception, "Setting invalid pred '" + sarg + "' for node " + node
+
+        if argmap[sarg] == None:
+            data['post_c'] = Bool(current + "_post_c")
+            data['post_i'] = Bool(current + "_post_i")
+            data['post_f'] = Bool(current + "_post_f")
+        else:
+            data['post_c'] = Bool(True) if 'c' in argmap[sarg] else Bool(current + "_post_c")
+            data['post_i'] = Bool(True) if 'i' in argmap[sarg] else Bool(current + "_post_i")
+            data['post_f'] = Bool(True) if 'f' in argmap[sarg] else Bool(current + "_post_f")
 
 def freeze_node (G, node):
     changes = 0
@@ -482,11 +490,15 @@ def colorize (G, nodelist):
         for (parent, child, data) in G.in_edges(nbunch=node, data=True):
             insec |= data['sec']
 
+        sec = insec | outsec
+        if 'sec' in n and n['sec'] != None:
+            sec |= n['sec']
+
         n['fontname'] = "Times Bold"
         n['fontcolor'] = "gray"
         n['style'] = "filled"
         n['gradientangle'] = "90"
-        n['fillcolor'] = "\"" + sec_color(insec | outsec) + "\""
+        n['fillcolor'] = "\"" + sec_color(sec) + "\""
 
 def check_input (G, node, args, param, threshold): 
     if args[param] > threshold:
@@ -533,12 +545,13 @@ def validate_graph (G):
 
         if kind == 'const':
 
+            pass
             # All outgoing environments must at least guarantee the const sec set
-            for (current, child, data) in G.out_edges(nbunch=node, data=True):
-                if "c" in n['sec'] and not "c" in data['sec']:
-                    print "ERROR: confidentiality not guaranteed between '" + current + "' and '" + child + "'"
-                    data['color'] = 'orange'
-                    data['extralabel'] = "\n[" + fmtsec(n['sec']) + ">" + fmtsec(data['sec']) + "]"
+            # for (current, child, data) in G.out_edges(nbunch=node, data=True):
+            #     if "c" in n['sec'] and not "c" in data['sec']:
+            #         print "ERROR: confidentiality not guaranteed between '" + current + "' and '" + child + "'"
+            #         data['color'] = 'orange'
+            #         data['extralabel'] = "\n[" + fmtsec(n['sec']) + ">" + fmtsec(data['sec']) + "]"
 
         elif kind == 'xform':
 
@@ -764,19 +777,247 @@ def write_graph(G, title):
     
     pd = nx.drawing.nx_pydot.to_pydot(G)
     pd.set_name("sdg")
-    #pd.set ("size", "11.7,8.3")
     pd.set ("splines", "ortho")
     pd.set ("forcelabels", "true")
     pd.set ("nodesep", "0.5")
     pd.set ("pack", "true")
     pd.set ("size", "15.6,10.7")
     pd.set ("label", title + "/" + str(iteration))
-    pd.set ("labelloc", "tl")
+    pd.set ("labelloc", "t")
     pd.write(out + ".dot")
     subprocess.check_output (["dot", "-T", "pdf", "-o", out, out + ".dot"]);
     pdfs.append(out)
-    os.remove (out + ".dot")
-    
+    #os.remove (out + ".dot")
+
+def set_out_c (G, node, sarg, value):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            data['out_var_c'] = value
+
+def set_out_i (G, node, sarg, value):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            data['out_var_i'] = value
+
+def set_out_f (G, node, sarg, value):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            data['out_var_f'] = value
+
+def get_out_c (G, node, sarg):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            return data['out_var_c']
+
+def get_out_i (G, node, sarg):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            return data['out_var_i']
+
+def get_out_f (G, node, sarg):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            return data['out_var_f']
+
+def get_in_c (G, node, darg):
+    for (current, child, data) in G.in_edges(nbunch=node, data=True):
+        if data['darg'] == darg:
+            return data['in_var_c']
+
+def get_in_i (G, node, darg):
+    for (current, child, data) in G.in_edges(nbunch=node, data=True):
+        if data['darg'] == darg:
+            return data['in_var_i']
+
+def get_in_f (G, node, darg):
+    for (current, child, data) in G.in_edges(nbunch=node, data=True):
+        if data['darg'] == darg:
+            return data['in_var_f']
+
+def init_in_vars(G, node, solver, darg):
+    for (parent, current, data) in G.in_edges(nbunch=node, data=True):
+        if data['darg'] == darg:
+            data['in_var_c'] = Bool(node + "_" + data['darg'] + "_in_c")
+            data['in_var_i'] = Bool(node + "_" + data['darg'] + "_in_i")
+            data['in_var_f'] = Bool(node + "_" + data['darg'] + "_in_f")
+
+            solver.assert_and_track (data['out_var_c'] == data['in_var_c'], node + "_" + darg + "_inout_c")
+            solver.assert_and_track (data['out_var_i'] == data['in_var_i'], node + "_" + darg + "_inout_i")
+            solver.assert_and_track (data['out_var_f'] == data['in_var_f'], node + "_" + darg + "_inout_f")
+
+def init_out_vars(G, node, solver, sarg):
+    for (current, child, data) in G.out_edges(nbunch=node, data=True):
+        if data['sarg'] == sarg:
+            data['out_var_c'] = Bool(node + "_" + data['darg'] + "_out_c")
+            data['out_var_i'] = Bool(node + "_" + data['darg'] + "_out_i")
+            data['out_var_f'] = Bool(node + "_" + data['darg'] + "_out_f")
+
+def secset (c, i, f):
+    result = sec_empty()
+    if c == True:
+        result |= sec_c()
+    if i == True:
+        result |= sec_i()
+    if f == True:
+        result |= sec_f()
+    return result
+
+def analyze_satisfiability (G):
+    s = Solver()
+    for node in nx.topological_sort (G):
+        analyze_sat (G, node, s)
+
+    if s.check() == sat:
+        print "Solution found"
+        m = s.model()
+        for (parent, child, data) in G.edges(data=True):
+            c = str(m.evaluate(data['out_var_c'])) == "True"
+            i = str(m.evaluate(data['out_var_i'])) == "True"
+            f = str(m.evaluate(data['out_var_f'])) == "True"
+            data['sec'] = secset (c, i, f)
+    else:
+        print s.unsat_core()
+        print "No solution"
+
+def analyze_sat (G, node, s):
+
+    n = G.node[node]
+    kind = n['kind']
+
+    if kind == "xform":
+
+        has_source = False
+        has_sink   = False
+
+        c = []
+        i = []
+        f = []
+
+        for (parent, current, data) in G.in_edges(nbunch=node, data=True):
+            has_source = True
+            init_in_vars (G, node, s, data['darg'])
+            c.append (get_in_c (G, node, data['darg']))
+            i.append (get_in_i (G, node, data['darg']))
+            f.append (get_in_f (G, node, data['darg']))
+
+        for (current, child, data) in G.out_edges(nbunch=node, data=True):
+            has_sink = True
+            init_out_vars (G, node, s, data['sarg'])
+            if c: set_out_c (G, node, data['sarg'], Or(c))
+            if i: set_out_i (G, node, data['sarg'], And(i))
+            if f: set_out_f (G, node, data['sarg'], And(f))
+
+        if has_source and has_sink:
+            # ordinary xforms don't have security labels
+            pass
+        elif has_source:
+            # a sink, assert all incoming edges to have at least the sinks security level
+            for (parent, current, data) in G.in_edges(nbunch=node, data=True):
+                sarg = data['sarg']
+                c = Bool(node + "_" + sarg + "_sink_c")
+                i = Bool(node + "_" + sarg + "_sink_c")
+                f = Bool(node + "_" + sarg + "_sink_c")
+                c = c if 'c' in n['sec'] else Not(c)
+                i = i if 'i' in n['sec'] else Not(i)
+                f = f if 'f' in n['sec'] else Not(f)
+                s.assert_and_track (Implies (data['in_var_c'], c), current + "_xform_" + sarg + "_in_c")
+                s.assert_and_track (Implies (data['in_var_i'], i), current + "_xform_" + sarg + "_in_i")
+                s.assert_and_track (Implies (data['in_var_f'], f), current + "_xform_" + sarg + "_in_f")
+
+        elif has_sink:
+            # a source, assert all outgoing edges to have at most the sources security level
+            for (current, child, data) in G.out_edges(nbunch=node, data=True):
+                darg = data['darg']
+                c = Bool(node + "_" + darg + "_source_c")
+                i = Bool(node + "_" + darg + "_source_c")
+                f = Bool(node + "_" + darg + "_source_c")
+                c = c if 'c' in n['sec'] else Not(c)
+                i = i if 'i' in n['sec'] else Not(i)
+                f = f if 'f' in n['sec'] else Not(f)
+                s.assert_and_track (Implies (c, data['out_var_c']), current + "_xform_" + darg + "_out_c")
+                s.assert_and_track (Implies (i, data['out_var_i']), current + "_xform_" + darg + "_out_i")
+                s.assert_and_track (Implies (f, data['out_var_f']), current + "_xform_" + darg + "_out_f")
+
+        else:
+            # no incoming and no outgoing edges? This is an error.
+            raise Exception, "xform with no edges: " + node
+
+    elif kind == "const":
+        init_out_vars (G, node, s, "const")
+
+    elif kind == "rand":
+        init_in_vars (G, node, s, "len")
+        init_out_vars (G, node, s, "data")
+
+        # Length input parameter needs integrity
+        s.assert_and_track (get_in_i (G, node, "len"), node + "_rand_len_in_i")
+
+        # Output data parameter provides confidentiality, integrity and freshness
+        s.assert_and_track (get_out_c (G, node, "data"), node + "_rand_data_out_c")
+        s.assert_and_track (get_out_i (G, node, "data"), node + "_rand_data_out_i")
+        s.assert_and_track (get_out_f (G, node, "data"), node + "_rand_data_out_f")
+
+    elif kind == "dhpub":
+        init_in_vars (G, node, s, "gen")
+        init_in_vars (G, node, s, "psec")
+        init_out_vars (G, node, s, "pub")
+        init_out_vars (G, node, s, "psec")
+
+        # Generator needs integrity
+        s.assert_and_track (get_in_i (G, node, "gen"), node + "_dhpub_gen_in_i")
+
+        # Secret psec requires confidentiality, integrity and freshness
+        s.assert_and_track (get_in_c (G, node, "psec"), node + "_dhpub_psec_in_c")
+        s.assert_and_track (get_in_i (G, node, "psec"), node + "_dhpub_psec_in_j")
+        s.assert_and_track (get_in_f (G, node, "psec"), node + "_dhpub_psec_in_f")
+
+        # Output secret psec only guarantees confidentiality, integrity
+        s.assert_and_track (get_out_c (G, node, "psec"), node + "_dhpub_psec_out_c")
+        s.assert_and_track (get_out_i (G, node, "psec"), node + "_dhpub_psec_out_i")
+
+    elif kind == "dhsec":
+        init_in_vars (G, node, s, "pub")
+        init_in_vars (G, node, s, "psec")
+        init_out_vars (G, node, s, "ssec")
+
+        # Input secret psec requires confidentiality and integrity
+        s.assert_and_track (get_in_c (G, node, "psec"), node + "_dhsec_psec_in_c")
+        s.assert_and_track (get_in_i (G, node, "psec"), node + "_dhsec_psec_in_i")
+
+        # Output secret ssec requires confidentiality
+        s.assert_and_track (get_out_c (G, node, "ssec"), node + "dhsec_ssec_out_c")
+        s.assert_and_track (get_in_i (G, node, "pub") == get_out_i (G, node, "ssec"), node + "dhsec_ssec_inout_i")
+        s.assert_and_track (get_in_f (G, node, "pub") == get_out_f (G, node, "ssec"), node + "dhsec_ssec_inout_f")
+
+    elif kind == "hash":
+        init_in_vars (G, node, s, "msg")
+        init_out_vars (G, node, s, "hash")
+
+        # FIXME: How about situations where confidentiality is removed by hashing (e.g. passwords)?
+        s.assert_and_track (Implies (get_in_c (G, node, "msg"), get_out_c (G, node, "hash")), node + "_hash_hash_out_c")
+        s.assert_and_track (Implies (get_in_i (G, node, "msg"), get_out_i (G, node, "hash")), node + "_hash_hash_out_i")
+        s.assert_and_track (Implies (get_in_f (G, node, "msg"), get_out_f (G, node, "hash")), node + "_hash_hash_out_f")
+
+    elif kind == "encrypt":
+        init_in_vars (G, node, s, "iv")
+        init_in_vars (G, node, s, "key")
+        init_in_vars (G, node, s, "plaintext")
+        init_out_vars (G, node, s, "ciphertext")
+
+        # Output ciphertext does not have confidentiality requirement if:
+        #       key is confidential and
+        #       key is of integrity and
+        #       iv is of integrity and
+        #       either key or iv are fresh
+        security  = And (get_in_c (G, node, "key"), get_in_i (G, node, "key"), get_in_i (G, node, "iv"))
+        freshness = Or (get_in_f (G, node, "iv"), get_in_f (G, node, "key"))
+        s.assert_and_track (And (security, freshness) == Not (get_out_c (G, node, "ciphertext")), node + "_encrypt" + "_ciphertext_c")
+        s.assert_and_track (Implies (get_in_i (G, node, "plaintext"), get_out_i (G, node, "ciphertext")), node + "_encrypt" + "_ciphertext_i")
+        s.assert_and_track (Implies (get_in_f (G, node, "plaintext"), get_out_f (G, node, "ciphertext")), node + "_encrypt" + "_ciphertext_f" )
+
+    else:
+        raise Exception, "Unhandled primitive '" + kind + "'"
+
 def main(args):
     global output
 
@@ -786,19 +1027,17 @@ def main(args):
     G = parse_graph (args.input[0])
     output = args.output[0]
 
-    count = 0 
-    iterations = 0
-    fw_changes = 0
-    bw_changes = 0
+    # bwc = analyze_backwards (G, args.incremental)
+    # print "Backwards changes: " + str(bwc)
 
-    bwc = analyze_backwards (G, args.incremental)
-    print "Backwards changes: " + str(bwc)
-
-    fwc = analyze_forwards(G, args.incremental)
-    print "Forwards changes: " + str(fwc)
+    # fwc = analyze_forwards(G, args.incremental)
+    # print "Forwards changes: " + str(fwc)
 
     validate_graph (G)
+    analyze_satisfiability(G)
     write_graph(G, "Final")
+
+    print "PDFs: " + str(pdfs)
 
     a = ["pdftk"] + pdfs + ["cat", "output", args.output[0]]
     subprocess.check_output (a)

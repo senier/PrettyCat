6745a204cad690ae0ed281a0b2915f5e8b212a3a
Alexander Senier <alexander.senier@tu-dresden.de>
2016-11-21 10:44:38 +0100

    Support multiple consecutive test messages

--- tests/testlib.py
+++ tests/testlib.py
@@ -1,6 +1,33 @@
 import sys
 from libspg import info, warn, err, SPG_base
 import libspg
+import time
+import threading
+
+class input_list (threading.Thread):
+
+    def __init__ (self, name, config, recvmethods):
+
+        super().__init__ ()
+
+        self.recvmethods = recvmethods
+        self.name        = name
+        self.config      = config
+
+        if not 'data' in config.attrib:
+            raise Exception ("No input data configured")
+
+        self.delay = 0
+        if 'delay' in config.attrib:
+            self.delay = float(config.attrib['delay'])
+
+        self.values = [x.strip() for x in config.attrib['data'].split(',')]
+
+    def run (self):
+
+        time.sleep(self.delay)
+        for v in self.values:
+            self.recvmethods['data'](v.encode())
 
 class output_check_fixed (SPG_base):
 
@@ -11,9 +38,10 @@ class output_check_fixed (SPG_base):
         if not 'result' in config.attrib:
             raise Exception ("No result set for output check")
 
-        self.value = str(config.attrib['result'])
+        self.values = [x.strip() for x in config.attrib['result'].split(',')]
 
     def recv_data (self, data):
-        if str(data) != self.value:
-            warn ("Output '" + str(data) + "' did not match expected value '" + str(self.value) + "'")
+        value = self.values.pop()
+        if str(data) != value:
+            warn ("Output '" + str(data) + "' did not match expected value '" + str(value) + "'")
             libspg.exitval = 1

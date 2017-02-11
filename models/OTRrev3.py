3c36e9a4bba36317ecef71e823f1f22776a58e9c
Alexander Senier <alexander.senier@tu-dresden.de>
2017-02-11 11:11:46 +0100

    xform_signature_r: Send signature to both outputs

--- models/OTRrev3.py
+++ models/OTRrev3.py
@@ -340,7 +340,8 @@ class xform_signature_r (libspg.SPG_base, libspg.MPI):
 
     def recv_sigm (self, sigm):
         (encrypted_sig, macd_signature) = self.decode_data (sigm)
-        self.send ('encrypted_signature', encrypted_sig)
+        self.send ('encrypted_signature#1', encrypted_sig)
+        self.send ('encrypted_signature#2', encrypted_sig)
         self.send ('macd_signature', macd_signature)
 
 class xform_split_x (libspg.SPG_base, libspg.MPI):

0d576b5357aa2cd6a3f6cd6ac0130da16617f7a1
Alexander Senier <alexander.senier@tu-dresden.de>
2017-03-07 09:26:45 +0100

    Implement unsat core text output

--- spg/z3exp.py
+++ spg/z3exp.py
@@ -151,3 +151,63 @@ class Z3Unsat (Z3Expr):
             self.unsat[name][inout][arg] = {}
 
         self.unsat[name][inout][arg][kind] = True
+
+class Z3Text (Z3Expr):
+
+    def __init__ (self, exp):
+        super().__init__ (exp)
+
+    def str (self, prefix = None, label = False):
+        self.prefix = prefix
+        self.label  = label
+        self.result = ""
+        self.__iterate__ (self.exp)
+        return self.result
+
+    def handle_and_elem (self, level, arg_no):
+        if arg_no != 0: self.result += " ∧ "
+        if level == 0: self.result += "\n   "
+
+    def handle_or_pre (self, level, num_args):
+        if num_args > 1: self.result += "("
+
+    def handle_or_elem (self, level, arg_no):
+        if arg_no != 0: self.result += " ∨ "
+
+    def handle_or_post  (self, level, num_args):
+        if num_args > 1: self.result += ")"
+
+    def handle_eq_op (self, level, num_args):
+        self.result += " ⇔ "
+
+    def handle_implies_op (self, level, num_args):
+        self.result += " ⇒ "
+
+    def handle_neg_op (self, level, num_args):
+        self.result += "¬"
+
+    def handle_invalid_op (self, level, num_args):
+        self.result += " ⊥ "
+
+    def handle_const_op (self, exp, level, num_args):
+
+        var = str(exp)
+
+        if var == "True" or var == "False":
+            self.result += var
+        else:
+            (name, inout, arg, kind) = var.split ('>')
+
+            textname = {"input": "", "output": "⁰", "intg": "₁", "conf": "₍"}
+
+            if kind != 'intg' and kind != 'conf':
+                raise Exception ("Invalid variable " + var + ": neither integrity nor confidentiality")
+
+            if inout != 'input' and inout != 'output':
+                raise Exception ("Invalid variable " + var + ": neither input nor output")
+
+            name = name.capitalize()
+            name = re.sub ('>', '\>', name)
+            name = re.sub (' ', '_', name)
+
+            self.result += name + textname[inout] + textname[kind]
